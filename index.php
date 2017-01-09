429656a92e13ca10205307d38530b6fcd4759eb9
nicjac <nicolas.jaccard@gmail.com>
2017-01-09 20:12:27 +0000

    Forgotten commit

--- index.php
+++ index.php
@@ -67,49 +67,7 @@
 
 <body>
 
-  <nav class="blue-grey lighten-2">
-    <div class="nav-wrapper container">
-      <a href="#!" class="brand-logo">Nicolas Jaccard</a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-<!--         <li><a href="#">Home</a></li>
-        <li><a href="#">My background</a></li>
-        <li><a href="#">Research</a></li>
-        <li><a href="#">Contacts</a></li> -->
-        <li><a href="http://www.twitter.com/nicjac/"><i class="mdi mdi-twitter-box"></i></a></li>
-        <li><a href="https://uk.linkedin.com/in/nicolasjaccard"><i class="mdi mdi-linkedin-box"></i></a></li>
-      </ul>
-<!--       <ul class="side-nav" id="mobile-demo">
-        <li><a href="#">Sass</a></li>
-        <li><a href="#">Components</a></li>
-        <li><a href="#">Javascript</a></li>
-        <li><a href="#">Mobile</a></li>
-      </ul> -->
-    </div>
-  </nav>
-
-  <div class="section fade-in" id="index-banner" style="display: block; opacity: 1;">
-    <div class="container">
-      <div class="row center">
-        <div class="col s12 m12">
-          <h2 class="header section-title">
-            <span>
-              About me
-            </span>
-          </h2>
-
-          <p class="flow-text">
-            Find out more about my <strong class="deep-purple-text lighten-1">academic background</strong>, <strong class="light-green-text lighten-1">experience</strong>, <strong class="blue-grey-text lighten-1">skills</strong>, and  <strong class="amber-text lignth-1">awards</strong> (<i class="material-icons">expand_more</i> items can be expanded).
-          </p>
-        </div>
-      </div>
-
-      <div class="row center">
-        <a class="waves-effect waves-light btn" href="Files/CV_NicolasJaccard_Web.pdf"><i class="material-icons left">archive</i>CV (PDF)</a>
-        <a class="waves-effect waves-light btn" href="https://uk.linkedin.com/in/nicolasjaccard" target="_blank"><i class="mdi mdi-linkedin-box left"></i>LinkedIn profile</a>
-      </div>
-    </div>
-  </div>
+<?php include 'topBanner.php';?>
 
   <div class="container">
     <div class="section">
